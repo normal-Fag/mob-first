a8d4931f610c159b1db3a84dff34f29f8227e1e0
normal-Fag <72517164+normal-Fag@users.noreply.github.com>
2021-10-31 17:22:09 +0700

    Complete promo section

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ const burger_btn = document.querySelector(".header__burger");
 const menu = document.querySelector(".menu");
 
 burger_btn.addEventListener('click', () => {
-	// burger_btn.classList.toggle('active');
 	menu.classList.toggle('active');
 	main_content.classList.toggle('blure');
 })
